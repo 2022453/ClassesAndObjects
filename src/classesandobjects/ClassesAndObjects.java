01981b4beb0ffcfe164b0663f0ee6f234762b9cf
2022453 <2022453@student.cct.ie>
2023-10-04 12:51:34 +0100

    Triangle sides in Arrays
    Made a triangle classe and 6 triangle objects, we tried to keep track of all the sides lengths in arrays, but that got really confusing.

--- src/classesandobjects/ClassesAndObjects.java
+++ src/classesandobjects/ClassesAndObjects.java
@@ -15,6 +15,38 @@ public class ClassesAndObjects {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Triangle tri1 = new Triangle();      
+        Triangle tri2 = new Triangle();
+        Triangle tri3 = new Triangle();
+        Triangle tri4 = new Triangle();
+        Triangle tri5 = new Triangle();
+        Triangle tri6 = new Triangle();
+        
+        int[] sideLenghts = new int [18];
+        //the lenghts of tril's sides will be first, then the lenghts of tri2's  sides etc 
+        
+
+        sideLenghts[0] = 5;
+        sideLenghts[1] = 8;
+        sideLenghts[2] = 9;
+        
+        
+       //tri5 (4,7,2)
+        sideLenghts[12] = 4;
+        sideLenghts[13] = 7;
+        sideLenghts[14] = 2;
+        
+        // we want to set the middle side of each triangle to be 3
+        sideLenghts[1] = 3;
+        sideLenghts[4] = 3;
+        sideLenghts[7] = 3;
+        sideLenghts[10] = 3;
+        sideLenghts[13] = 3;
+        sideLenghts[16] = 3;
+       
+          
+
     }
     
 }
