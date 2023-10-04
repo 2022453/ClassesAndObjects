566e7275fb02448052aa682128beb549eed082db
2022453 <2022453@student.cct.ie>
2023-10-04 13:11:49 +0100

    Made a new shapes package and moved the Triangle class into it.

---
+++ src/shapes/Triangle.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package shapes;
+
+/**
+ *
+ * @author paulooliveira
+ */
+public class Triangle {
+    
+    
+    int side1;
+    int side2;
+    int side3;
+          
+    
+    
+}
